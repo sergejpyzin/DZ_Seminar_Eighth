4f6fd61a9d406350f551332b6ae837f1816e68c0
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-03-09 15:34:18 +0300

    Finished task_60

--- task_60/Program.cs
+++ task_60/Program.cs
@@ -2,9 +2,70 @@
 {
     internal class Program
     {
+        static int GetConvertUserMassadge(string userMassedge)
+        {
+            int convertNumber = 0;
+            while (true)
+            {
+                Console.WriteLine(userMassedge);
+                bool checkUserMassedge = int.TryParse(Console.ReadLine(), out convertNumber);
+                if (checkUserMassedge) return convertNumber;
+                else Console.WriteLine("Ошибка! Попробуйте еще раз.");
+            }
+        }
+
+        static void PrintMatrix(int[,,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matrix.GetLength(2); k++)
+                    {
+                        Console.Write($"{matrix[i, j, k]} ({i}, {j}, {k}) ");
+                    }
+                    Console.WriteLine();
+                }                
+            }
+            Console.WriteLine();
+        }
+        static int[,,] GetRandomMatrix(int row, int colums, int height, int leftBoarder, int rightBoarder)
+        {
+            int[,,] matrix = new int[row, colums, height];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    for (int k = 0; k< matrix.GetLength(2); k++)
+                    {
+                        matrix[i, j, k] = Random.Shared.Next(leftBoarder, rightBoarder + 1);
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        static int[,] GetResultMatrixMultiplication(int[,] firstMatrix, int[,] secondMatrix)
+        {
+            int[,] arrayResult = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
+            for (int i = 0; i < arrayResult.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayResult.GetLength(1); j++)
+                {
+                    arrayResult[i, j] = firstMatrix[i, j] * secondMatrix[i, j];
+                }
+            }
+            return arrayResult;
+        }
+        //        Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+        //        Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+        //        массив размером 2 x 2 x 2
+        //        12(0,0,0) 22(0,0,1)
+        //        45(1,0,0) 53(1,0,1)
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[,,] matrix = GetRandomMatrix(3, 3, 3, 10, 98);
+            PrintMatrix(matrix);
         }
     }
 }
